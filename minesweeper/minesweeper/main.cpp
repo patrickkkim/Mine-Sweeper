9e08e74b4b6f1c735fc51f4ef4137b48ad07b184
김승훈 <patkim97@gmail.com>
2020-01-08 19:52:53 +0900

    기본적인 그리드 디자인 적용

--- minesweeper/minesweeper/main.cpp
+++ minesweeper/minesweeper/main.cpp
@@ -1,12 +1,42 @@
-#include <QtWidgets/QApplication>
-#include <QtWidgets/QWidget>
+#include <QApplication>
+#include <QWidget>
+#include <QPushButton>
+#include <QGridLayout>
+
+class CustomButton : public QWidget {
+public:
+	CustomButton(QWidget *parent = 0) {
+		QPushButton *mineButton = new QPushButton("X", this);
+		
+		connect(mineButton, &QPushButton::clicked, qApp, &QApplication::quit);
+	}
+};
+
+class Grid : public QWidget {
+public:
+	Grid(QWidget *parent = 0) {
+		QGridLayout *grid = new QGridLayout(this);
+		grid->setSpacing(0);
+
+		int pos = 0;
+
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < 10; j++) {
+				QPushButton *btn = new QPushButton(this);
+				btn->setFixedSize(25, 25);
+				grid->addWidget(btn, i, j);
+				pos++;
+			}
+		}
+	}
+};
 
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
 
-	QWidget window;
-	window.resize(500, 350);
+	Grid window;
+	window.resize(300, 200);
 	window.setWindowTitle("Mine Sweeper");
 	window.show();
 
