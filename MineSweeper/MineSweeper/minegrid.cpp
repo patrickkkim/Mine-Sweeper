6d82d73bc340ec7343f2d2fe18d854682c20755b
김승훈 <patkim97@gmail.com>
2020-01-16 23:55:12 +0900

    Revert "우클릭 기능 추가 및 빈칸제거기능 향상"
    This reverts commit a03e356d72e6b19f9d399d340c75dd0f1681e04a.

--- MineSweeper/MineSweeper/minegrid.cpp
+++ MineSweeper/MineSweeper/minegrid.cpp
@@ -1,8 +1,6 @@
 #include <QLabel>
 #include <QPushButton>
-#include <QDebug>
 #include "minegrid.h"
-#include "event.h"
 
 MineGrid::MineGrid(int r, int c, int maxMine, QWidget *parent) {
 	row = r, col = c;
@@ -15,10 +13,9 @@ MineGrid::MineGrid(int r, int c, int maxMine, QWidget *parent) {
 	int pos = 0;
 	for (int i = 0; i < col; i++) {
 		for (int j = 0; j < row; j++) {
-			QCustomPushButton *button = new QCustomPushButton(this);
+			QPushButton *button = new QPushButton(this);
 			button->setFixedSize(28, 28);
-			connect(button, &QCustomPushButton::clicked, grid, [this, i, j] { leftBtnClicked(i, j); });
-			connect(button, &QCustomPushButton::rightClicked, grid, [this, i, j] { RightBtnClicked(i, j); });
+			connect(button, &QPushButton::clicked, grid, [this, i, j] { buttonClicked(i, j); });
 			grid->addWidget(button, i, j);
 			pos++;
 		}
@@ -26,19 +23,21 @@ MineGrid::MineGrid(int r, int c, int maxMine, QWidget *parent) {
 }
 
 // 버튼이 눌렷을시 버튼을 없애고 그자리에 지뢰데이터를 표시함
-void MineGrid::leftBtnClicked(int x, int y) {
+void MineGrid::buttonClicked(int x, int y) {
 	if (isMine(x, y)) {
-		openWidget(x, y);
+		openBtn(x, y);
 		QLabel *label = new QLabel(this);
-		QPixmap minePixmap("../icons/Mine.png");
-		label->setPixmap(minePixmap.scaled(26, 26, Qt::KeepAspectRatio));
+		QPixmap pixmap("../icons/Mine.png");
+		label->setPixmap(pixmap.scaled(26, 26, Qt::KeepAspectRatio));
 		grid->addWidget(label, x, y);
 	}
 	else {
 		int adjCount = data->adjData[x][y];
 		if (adjCount != 0) {
-			openWidget(x, y);
-			addAdjData(x, y, adjCount);
+			openBtn(x, y);
+			QLabel *label = new QLabel(QString::number(adjCount), this);
+			label->setAlignment(Qt::AlignCenter);
+			grid->addWidget(label, x, y);
 		}
 		else {
 			openAllBlankBtn(x, y);
@@ -46,29 +45,13 @@ void MineGrid::leftBtnClicked(int x, int y) {
 	}
 }
 
-void MineGrid::RightBtnClicked(int x, int y) {
-	if (isButton(x, y) && isFlagged(x, y) == false) {
-		flagBtn(x, y);
-	}
-}
-
-void MineGrid::addAdjData(int x, int y, int count) {
-	QLabel *label = new QLabel(QString::number(count), this);
-	label->setAlignment(Qt::AlignCenter);
-	grid->addWidget(label, x, y);
-}
-
 // 해당 빈칸과 서로 접하고있는 모든 빈칸을 열어놓음
 void MineGrid::openAllBlankBtn(int x, int y) {
-	if (isButton(x, y) == false) {
+	if (isButton(x, y) == false || data->adjData[x][y] != 0) {
 		return;
 	}
-	else if (data->adjData[x][y] != 0 && isMine(x, y) == false) {
-		openWidget(x, y);
-		addAdjData(x, y, data->adjData[x][y]);
-	}
 	else {
-		openWidget(x, y);
+		openBtn(x, y);
 		QLabel *label = new QLabel(this);
 		grid->addWidget(label, x, y);
 		openAllBlankBtn(x - 1, y);
@@ -79,40 +62,10 @@ void MineGrid::openAllBlankBtn(int x, int y) {
 }
 
 // 해당 버튼을 열음
-void MineGrid::openWidget(int x, int y) {
-	QWidget *widget = grid->itemAtPosition(x, y)->widget();
-	layout()->removeWidget(widget);
-	delete widget;
-
-	QLayoutItem *type = grid->itemAtPosition(x, y);
-	if (type != 0) {
-		widget = type->widget();
-		layout()->removeWidget(widget);
-		delete widget;
-	}
-}
-
-void MineGrid::flagBtn(int x, int y) {
-	QPixmap flagPixMap("../icons/flag.png");
-	QCustomLabel *label = new QCustomLabel(this);
-	connect(label, &QCustomLabel::rightClicked, grid, [this, x, y] { unFlag(x, y); });
-	label->setPixmap(flagPixMap.scaled(26, 26, Qt::KeepAspectRatio));
-	grid->addWidget(label, x, y);
-}
-
-void MineGrid::unFlag(int x, int y) {
-	QWidget *flag = grid->itemAtPosition(x, y)->widget();
-	flag->raise();
-}
-
-bool MineGrid::isFlagged(int x, int y) {
-	QCustomLabel *type = qobject_cast<QCustomLabel*>(grid->itemAtPosition(x, y)->widget());
-	if (type != nullptr) {
-		return true;
-	}
-	else {
-		return false;
-	}
+void MineGrid::openBtn(int x, int y) {
+	QWidget *btn = grid->itemAtPosition(x, y)->widget();
+	layout()->removeWidget(btn);
+	delete btn;
 }
 
 // 해당 칸에 버튼이 있는지 확인
@@ -120,12 +73,13 @@ bool MineGrid::isButton(int x, int y) {
 	if (x < 0 || y < 0 || x >= col || y >= row) {
 		return false;
 	}
+
 	QPushButton *type = qobject_cast<QPushButton*>(grid->itemAtPosition(x, y)->widget());
-	if (type != nullptr) {
-		return true;
+	if (type == nullptr) {
+		return false;
 	}
 	else {
-		return false;
+		return true;
 	}
 }
 
--- MineSweeper/MineSweeper/minegrid.h
+++ MineSweeper/MineSweeper/minegrid.h
@@ -10,18 +10,13 @@ private:
 	Data *data;
 	QGridLayout *grid;
 
-	void leftBtnClicked(int x, int y);
-	void RightBtnClicked(int x, int y);
+	void buttonClicked(int x, int y);
 	void openAllBlankBtn(int x, int y);
-	void openWidget(int x, int y);
-	void flagBtn(int x, int y);
-	void unFlag(int x, int y);
-	bool isFlagged(int x, int y);
+	void openBtn(int x, int y);
 	bool isButton(int x, int y);
 	bool isMine(int x, int y);
 
 public:
-	void addAdjData(int x, int y, int count);
 	MineGrid(int r, int c, int maxMine, QWidget *parent = 0);
 	//bool isGameOver();
 };
